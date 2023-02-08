992961041d75321317d68531f20ed78a57137015
Carlos Maldonado <carlosmaldonado@MacBook-Pro-de-Carlos.local>
2023-02-07 19:49:29 -0800

    shop-app-init

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,30 @@
 import 'package:flutter/material.dart';
-import 'screens/meal_detail_screen.dart';
-import 'screens/category_meals_screen.dart';
-import 'screens/tab_screen.dart';
-import 'screens/filter_screen.dart';
-
-import './dummy_data.dart';
-import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Map<String, bool> _filters = {
-    'gluten': false,
-    'lactose': false,
-    'vegan': false,
-    'vegetarian': false,
-  };
-
-  List<Meal> _availableMeals = DUMMY_MEALS;
-
-  void _setFilters(Map<String, bool> filterData) {
-    setState(() {
-      _filters = filterData;
-      _availableMeals = DUMMY_MEALS.where((meal) {
-        if (_filters['gluten'] && !meal.isGlutenFree) return false;
-        if (_filters['lactose'] && !meal.isLactoseFree) return false;
-        if (_filters['vegan'] && !meal.isVegan) return false;
-        if (_filters['vegetarian'] && !meal.isVegetarian) return false;
-
-        return true;
-      });
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'MyShop',
       theme: ThemeData(
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-              ),
-          primarySwatch: Colors.pink,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1)),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => TabsScreen(),
-        CategoryMealsScreen.routeName: (context) =>
-            CategoryMealsScreen(_availableMeals),
-        MealDetailScreen.routeName: (context) => MealDetailScreen(),
-        FilterScreen.routeName: (context) => FilterScreen(_setFilters),
-      },
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('DeliMeals'),
-//       ),
-//       body: Center(
-//         child: Text('Navigation Time!'),
-//       ),
-//     );
-//   }
-// }
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s build a shop!'),
+      ),
+    );
+  }
+}
